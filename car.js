bbd825eb1c81f3eea8f79a0896d49d1d8b1f87f7
Troy Crawford <tacrawford91@gmail.com>
2018-02-27 19:14:01 -0600

    Added heroku parts

--- car.js
+++ car.js
@@ -3,7 +3,8 @@ var bodyParser = require("body-parser");
 var path = require("path");
 
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
